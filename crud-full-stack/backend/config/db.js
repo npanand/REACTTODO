06f93f3adb13056ee1f60b948ffe28aed0cac98b
npanand <npanandan4@gmail.com>
2023-09-14 15:25:06 +0530

    cors install

--- crud-full-stack/backend/config/db.js
+++ crud-full-stack/backend/config/db.js
@@ -4,7 +4,7 @@ const color = require("colors");
 const connectDB = async () => {
   try {
     const connection = await mongoose.connect(
-      "mongodb://host.docker.internal:27017/authenticator"
+      process.env.db
     );
     console.log(
       `Mongoose connected: ${connection.connection.host}`.cyan.underline
--- crud-full-stack/backend/server.js
+++ crud-full-stack/backend/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const db = require("./config/db");
-
+const cors=require("cors");
 db();
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
